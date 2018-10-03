5b6ce00502a7ab09eebaa8aff391e5af118c4890
Yunling Zhang <yun313350095@gmail.com>
2018-10-03 17:45:08 +0900

    Just save

---
+++ components/AppHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <el-menu
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#eef1f6"
+      text-color="#303133"
+      active-text-color="#409eff">
+      <el-menu-item index="logo" class="el-header-logo">Noonde</el-menu-item>
+      <el-menu-item index="login" class="el-header-item-login">{{ $t(`components.app-header.login`) }}</el-menu-item>
+      <el-submenu index="lang" class="el-header-item-lang">
+        <template slot="title" >{{ $t(`components.app-header.lang.zh-CN`) }}</template>
+        <el-menu-item index="zh-CN">{{ $t(`components.app-header.lang.zh-CN`) }}</el-menu-item>
+        <el-menu-item index="en-US">{{ $t(`components.app-header.lang.en-US`) }}</el-menu-item>
+        <el-menu-item index="ja">{{ $t(`components.app-header.lang.ja`) }}</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<!-- ============================================================================ -->
+<style lang="stylus">
+  .el-menu-demo
+    border-bottom: 0 !important
+  .el-header-logo
+    font-size: 20px
+  .el-header-item-login
+    float: right !important
+    font-size: 17px
+  .el-header-item-lang
+    float: right !important
+</style>
+<!-- ============================================================================ -->
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: '1',
+      langs: {
+        'zh-CN': '中文',
+        'en-US': 'English',
+        'ja': '日本語'
+      }
+    }
+  },
+  computed: {
+    lang() {
+      console.log(this.$route.path.split('/')[1])
+      return this.$route.path.split('/')[1] || 'zh-CN';
+     },
+   displayedLang() {
+     return this.langs[this.lang] || '中文';
+   },
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,8 +1,39 @@
 export const state = () => ({
   base: {
    locale: {
-     locales: ['ja', 'en', 'zh-CN'],
+     locales: ['ja', 'en-US', 'zh-CN'],
      selected: 'zh-CN'
    }
  }
 })
+
+
+export const mutations = {
+  // usage: this.$store.commit('update', ['some.path.to.data', value])
+  // you can also use for array like this.$store.commit('update', ['some.path.1.to.data', value])
+  // but the changes might not be detected by computed methods
+  // update the whole array in that case
+  update (state, array) {
+    // keys: ['some', 'path', 'to', 'data']
+    let keys = array[0].split('.')
+    // lastKey: 'data'; keys: ['some', 'path', 'to']
+    const lastKey = keys.pop()
+    // data: value
+    let data = array[1]
+    // dig into state by keys
+    // result: initialize by state at first
+    const pointer = keys.reduce((result, key) => {
+      // key: 'some'
+      if (key.length === 0) return result || {}
+      // '1' => formattedKey: 1; 'some' => formattedKey: 'some'
+      const formattedKey = isNaN(key) ? key : parseInt(key)
+      // if the key 'some' exists in result, next result: result['some']
+      // if not, set result['some'] = {}
+      return result[formattedKey] || {}
+    }, state)
+
+    const isObject = pointer[lastKey] instanceof Object && pointer[lastKey].constructor === Object
+    // set value to the specified key
+    pointer[lastKey] = isObject ? { ...pointer[lastKey], ...data } : data
+  }
+}
