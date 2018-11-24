39c932a17de45ec16bb5b94bd6f31537400bcbb8
Yunling Zhang <yun313350095@gmail.com>
2018-11-25 03:02:41 +0900

    just save for vuetify

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,73 +1,66 @@
 <template>
-  <div>
-    <el-menu
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-      background-color="#eef1f6"
-      text-color="#303133"
-      active-text-color="#409eff">
-      <el-menu-item index="logo" class="el-header-logo">Noonde</el-menu-item>
-      <el-menu-item index="login" class="el-header-item-login">{{ $t(`components.app-header.login`) }}</el-menu-item>
-      <el-submenu index="lang" class="el-header-item-lang">
-        <template slot="title" >{{ displayedLang }}</template>
-        <el-menu-item index="zh-CN">{{ $t(`components.app-header.lang.zh-CN`) }}</el-menu-item>
-        <el-menu-item index="en-US">{{ $t(`components.app-header.lang.en-US`) }}</el-menu-item>
-        <el-menu-item index="ja">{{ $t(`components.app-header.lang.ja`) }}</el-menu-item>
-      </el-submenu>
-    </el-menu>
-  </div>
+    <div>
+      <v-card flat tile>
+        <v-toolbar dense color ="blue-grey lighten-5">
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title>Noonde</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu offset-y bottom left attach>
+            <v-btn slot="activator" flat style="min-width: 48px">
+              <v-img :src="`language/${currentLanguage.country}.png`" width="24px">
+              </v-img>
+            </v-btn>
+            <v-list light>
+              <v-list-tile avatar v-for="language in languages" :key="language.locale" @click="changeLanguage(context, language.locale)">
+                <v-list-tile-avatar tile size="24px">
+                  <v-img  :src="`language/${language.country}.png`" width="24px"></v-img>
+                </v-list-tile-avatar>
+                <v-list-tile-title>{{language.name}}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+          <v-btn icon>
+            <v-icon>favorite</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>more_vert</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-card>
+    </div>
 </template>
 
 <!-- ============================================================================ -->
-<style lang="stylus">
-  .el-menu-demo
-    border-bottom: 0 !important
-  .el-header-logo
-    font-size: 20px
-  .el-header-item-login
-    float: right !important
-    font-size: 17px
-  .el-header-item-lang
-    float: right !important
-</style>
-<!-- ============================================================================ -->
-
 <script>
-import mixins from '~/utils/mixins/shared'
-
-export default {
-  mixins: [mixins],
-  data () {
-    return {
-      context: this,
-      activeIndex: '1',
-      langs: {
-        'zh-CN': '中文',
-        'en-US': 'English',
-        'ja': '日本語'
+  import mixins from '~/utils/mixins/shared'
+  export default {
+    mixins: [mixins],
+    data() {
+      return {
+        context: this,
+        languages: [
+          {"name": "English", "locale": "en-US", "country": "us"},
+          {"name": "简体中文", "locale": "zh-CN", "country": "cn"},
+          {"name": "日本語",   "locale": "ja",    "country": "jp"},
+        ],
       }
-    }
-  },
-  computed: {
-    lang() {
-      return this.$store.state.base.locale.selected;
     },
-   displayedLang() {
-     return this.langs[this.lang];
-   },
-   fullPath () {
-     return this.$store.state.base.layout.fullPath
-   },
-  },
-  methods: {
-    handleSelect (key, keyPath) {
-      switch (keyPath[0]) {
-        case 'lang':
-          this.changeLanguage(this.context, key)
-          break;
-      }
+    computed: {
+      currentLanguage () {
+        return this.languages.find(l => l.locale === this.$i18n.locale)
+      },
+      fullPath () {
+        return this.$store.state.base.layout.fullPath
+      },
+    },
+    methods: {
     }
   }
-}
 </script>
+<!-- ============================================================================ -->
+
+
+<style>
+
+</style>
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <el-header>
-      <app-header/>
-    </el-header>
+    <app-header/>
     <nuxt/>
   </div>
 </template>
