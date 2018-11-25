f4b7e2c4035e69f72adf3f5f7bb2b2e828dd28c0
Yunling Zhang <yun313350095@gmail.com>
2018-11-26 00:17:44 +0900

    just save

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,6 @@
           <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
           <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
           <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
-          <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
         </el-row>
       </div>
     </el-main>
